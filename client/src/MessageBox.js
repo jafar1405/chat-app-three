70b48d47ce2c62958a13f1be085eeb7f214ee300
Mohammad Jafaroddin <MJ36969@GSK.SATYAMBPO.COM>
2019-05-03 12:01:28 +0530

    chat send button functionality added

--- client/src/MessageBox.js
+++ client/src/MessageBox.js
@@ -3,19 +3,24 @@ import React from 'react';
 function MessageBox(props){
 
     const message = (e) => {
-        if(e.keyCode === 13){
-            e.preventDefault()
-            props.getMessage(e.target.value)
-            e.target.value = ""
-        }
+        console.log(e.target.message.value)
+        e.preventDefault()
+        props.getMessage(e.target.message.value)
+        e.target.message.value = ""
+        
     }
     return(
         <div>
             <div className="type_msg">
                         <div className="input_msg_write">
                         {/* <textarea ></textarea> */}
-                        <input onKeyDown={message} type="text" className="write_msg" placeholder="Type a message"/>
-                        <button className="msg_send_btn" type="button"><i className="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+                            <form onSubmit={message}>
+                                <input type="text" id="message" className="write_msg" placeholder="Type a message"/>
+                                <button className="msg_send_btn" id="enter" type="submit">
+                                    <i className="fa fa-paper-plane-o" aria-hidden="true"></i>
+                                    send
+                                </button>
+                            </form>
                         </div>
                     </div>
             
